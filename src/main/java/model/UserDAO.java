573b0b97328c20fcd2a9daf1f8517d80edd1dcf7
mana SATO <ssstttooo26@gmail.com>
2025-11-25 15:58:46 +0900

    READMEの作成

--- src/main/java/model/UserDAO.java
+++ src/main/java/model/UserDAO.java
@@ -10,28 +10,28 @@ public class UserDAO {
         String sql = "INSERT INTO user_table(USER_ID, NAME, PASSWORD, ROLE) VALUES (?, ?, ?, ?)";
         try(Connection conn = DBConnection.getConnection("shift_db")){
         if (conn == null) {
-            System.out.println("DB接続失敗: conn が null");
+//            System.out.println("DB接続失敗: conn が null");
             return false;
         }
         
-        System.out.println("DAO: DB接続成功");
+//        System.out.println("DAO: DB接続成功");
 
 		try (
              PreparedStatement pStmt = conn.prepareStatement(sql)) {
-			System.out.println("DAO: SQL準備成功");
+//			System.out.println("DAO: SQL準備成功");
             pStmt.setString(1, user.getUserId());
             pStmt.setString(2, user.getName());
             pStmt.setString(3, user.getPass());
             pStmt.setString(4, user.getRole());
 
             int result = pStmt.executeUpdate();
-            System.out.println("登録結果: " + result);
+//            System.out.println("登録結果: " + result);
 
             return result == 1;
 		}
 
         } catch (SQLException e) {
-        	System.out.println("SQLエラー: " + e.getMessage());
+//        	System.out.println("SQLエラー: " + e.getMessage());
             e.printStackTrace();
             return false;
         }
